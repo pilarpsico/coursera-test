447fff37e6e4a3bb116994fc27b534248672990d
pilarpsico <pillastraead@gmail.com>
2024-03-11 12:05:11 -0300

    exlcuir

--- restaurante/js/script.js
+++
@@ -1,8 +0,0 @@
-function abrirNav(){
-    document.getElementById("menuOculto").style.width="150px";
-    document.getElementById("principal").style.marginRight="150px";
-}
-function fecharNav(){
-    document.getElementById("menuOculto").style.width="0";
-    document.getElementById("principal").style.marginRight="0";
-}
