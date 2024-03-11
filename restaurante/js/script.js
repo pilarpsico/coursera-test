090fa98f52aea9e737c58e99c328a3062c153b59
pilarpsico <pillastraead@gmail.com>
2024-03-10 21:21:43 -0300

    atualizando

---
+++ restaurante/js/script.js
@@ -0,0 +1,8 @@
+function abrirNav(){
+    document.getElementById("menuOculto").style.width="150px";
+    document.getElementById("principal").style.marginRight="150px";
+}
+function fecharNav(){
+    document.getElementById("menuOculto").style.width="0";
+    document.getElementById("principal").style.marginRight="0";
+}
